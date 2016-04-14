75d1d372edfa04e69bdcc1d7d356329b58aa3d77
UAkbar <p14027507@myemail.dmu.ac.uk>
2016-04-14 14:55:02 +0100

    Test

--- EmailBackEnd/frmMain.Designer.cs
+++ EmailBackEnd/frmMain.Designer.cs
@@ -52,17 +52,19 @@
             // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(12, 21);
+            this.lblTitle.Location = new System.Drawing.Point(18, 32);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(167, 55);
+            this.lblTitle.Size = new System.Drawing.Size(253, 82);
             this.lblTitle.TabIndex = 8;
             this.lblTitle.Text = "Emails";
             // 
             // btnSendEmail
             // 
-            this.btnSendEmail.Location = new System.Drawing.Point(534, 21);
+            this.btnSendEmail.Location = new System.Drawing.Point(801, 32);
+            this.btnSendEmail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSendEmail.Name = "btnSendEmail";
-            this.btnSendEmail.Size = new System.Drawing.Size(103, 41);
+            this.btnSendEmail.Size = new System.Drawing.Size(154, 63);
             this.btnSendEmail.TabIndex = 9;
             this.btnSendEmail.Text = "Send Email";
             this.btnSendEmail.UseVisualStyleBackColor = true;
@@ -76,16 +78,18 @@
             "By Earliest",
             "Favourites First",
             "Alphabetical"});
-            this.cmbFilter.Location = new System.Drawing.Point(22, 110);
+            this.cmbFilter.Location = new System.Drawing.Point(33, 169);
+            this.cmbFilter.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cmbFilter.Name = "cmbFilter";
-            this.cmbFilter.Size = new System.Drawing.Size(121, 21);
+            this.cmbFilter.Size = new System.Drawing.Size(180, 28);
             this.cmbFilter.TabIndex = 10;
             // 
             // btnFilter
             // 
-            this.btnFilter.Location = new System.Drawing.Point(160, 110);
+            this.btnFilter.Location = new System.Drawing.Point(240, 169);
+            this.btnFilter.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnFilter.Name = "btnFilter";
-            this.btnFilter.Size = new System.Drawing.Size(88, 22);
+            this.btnFilter.Size = new System.Drawing.Size(132, 34);
             this.btnFilter.TabIndex = 11;
             this.btnFilter.Text = "Filter";
             this.btnFilter.UseVisualStyleBackColor = true;
@@ -93,9 +97,10 @@
             // 
             // btnGroups
             // 
-            this.btnGroups.Location = new System.Drawing.Point(455, 108);
+            this.btnGroups.Location = new System.Drawing.Point(682, 166);
+            this.btnGroups.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnGroups.Name = "btnGroups";
-            this.btnGroups.Size = new System.Drawing.Size(88, 22);
+            this.btnGroups.Size = new System.Drawing.Size(132, 34);
             this.btnGroups.TabIndex = 12;
             this.btnGroups.Text = "Groups";
             this.btnGroups.UseVisualStyleBackColor = true;
@@ -103,9 +108,10 @@
             // 
             // btnContacts
             // 
-            this.btnContacts.Location = new System.Drawing.Point(549, 108);
+            this.btnContacts.Location = new System.Drawing.Point(824, 166);
+            this.btnContacts.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnContacts.Name = "btnContacts";
-            this.btnContacts.Size = new System.Drawing.Size(88, 22);
+            this.btnContacts.Size = new System.Drawing.Size(132, 34);
             this.btnContacts.TabIndex = 13;
             this.btnContacts.Text = "Contacts";
             this.btnContacts.UseVisualStyleBackColor = true;
@@ -113,9 +119,10 @@
             // 
             // btnReturn
             // 
-            this.btnReturn.Location = new System.Drawing.Point(12, 442);
+            this.btnReturn.Location = new System.Drawing.Point(18, 680);
+            this.btnReturn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnReturn.Name = "btnReturn";
-            this.btnReturn.Size = new System.Drawing.Size(103, 41);
+            this.btnReturn.Size = new System.Drawing.Size(154, 63);
             this.btnReturn.TabIndex = 14;
             this.btnReturn.Text = "Return";
             this.btnReturn.UseVisualStyleBackColor = true;
@@ -129,9 +136,10 @@
             this.fromDataGridViewTextBoxColumn,
             this.subjectDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.emailsBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(22, 152);
+            this.dataGridView1.Location = new System.Drawing.Point(34, 234);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(600, 268);
+            this.dataGridView1.Size = new System.Drawing.Size(900, 412);
             this.dataGridView1.TabIndex = 15;
             // 
             // dateDataGridViewTextBoxColumn
@@ -171,9 +179,9 @@
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(649, 495);
+            this.ClientSize = new System.Drawing.Size(974, 762);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnReturn);
             this.Controls.Add(this.btnContacts);
@@ -182,6 +190,7 @@
             this.Controls.Add(this.cmbFilter);
             this.Controls.Add(this.btnSendEmail);
             this.Controls.Add(this.lblTitle);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmMain";
             this.Text = "frmMain";
             this.Load += new System.EventHandler(this.frmMain_Load);
